49fc6c867a3d51bca58e8e2ce3b9d1467557b913
Hunter Abshire <120337190+Hunter-Abshire@users.noreply.github.com>
2024-01-03 17:19:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function SendMail() {
     var params = {
         from_name: document.getElementById("fullName").value,
         Email_Id: document.getElementById("email_id").value,
+        phone: document.getElementById("phone").value,
         message: document.getElementById("message").value
     }
     emailjs.send("service_o8xl6sx", "template_dwwjjjs", params).then(function (res){
